34f4f30ab7bd786326c66847496a3e93d8647fab
Ang <isoboy@gmail.com>
2016-07-14 04:10:03 +1000

    operator overloading and ostream overloading

---
+++ OperatorOverloading/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+
+class Complex {
+
+  double real;
+  double imag;
+
+public:
+  Complex (double r, double i):
+  real(r),imag(i)
+  {
+
+  }
+
+  friend Complex operator+(const Complex &c1, const Complex &c2)
+  {
+    return Complex(c1.real+c2.real,c1.imag+c2.imag);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Complex& c)
+  {
+        os << "[" << c.real << "," << c.imag << "]";
+        return os;
+  }
+
+  double mag()
+  {
+    return std::sqrt(real*real + imag*imag);
+  }
+  double ang()
+  {
+    return std::atan(imag/real);
+  }
+};
+
+const double pi = 3.1415926535897;
+int main (int argc, char** argv)
+{
+
+  Complex a(0,1);
+  Complex b(std::sqrt(3),0);
+  // Complex c(1,1);
+  Complex c = a+b;
+
+  std::cout<<"c = " << c << "\n";
+  std::cout<<"|c| = " << c.mag() << "\n";
+  std::cout<<"angle c = " << c.ang() << "rad or " << c.ang()*180/pi << "deg" << "\n";
+
+
+  return 0;
+}
