a35c85307048e89f95d609e16712f8c0dd461e42
bot1131357 <yuang86>
2016-07-19 23:23:22 +1000

    Added unary operator overloading

--- OperatorOverloading/main.cpp
+++ OperatorOverloading/main.cpp
@@ -17,6 +17,38 @@ public:
   {
     return Complex(c1.real+c2.real,c1.imag+c2.imag);
   }
+  Complex & operator++()//pre increment
+  {
+  	++real;
+    return *this;
+  }
+
+  Complex & operator++(int)//post increment
+  {
+  	real++;
+    return *this;
+  }
+
+  Complex & operator--()
+  {
+  	--real;
+    return *this;
+  }
+
+  Complex & operator--(int)
+  {
+  	real--;
+    return *this;
+  }
+
+  friend bool operator==(const Complex &c1, const Complex &c2)
+  {
+    return (c1.real==c2.real && c1.imag==c2.imag);  	
+  }
+  friend bool operator!=(const Complex &c1, const Complex &c2)
+  {
+    return !(c1==c2);  	
+  }
 
   friend std::ostream& operator<<(std::ostream& os, const Complex& c)
   {
@@ -43,10 +75,20 @@ int main (int argc, char** argv)
   // Complex c(1,1);
   Complex c = a+b;
 
+  std::cout<<"a = " << a << "\n";
+  std::cout<<"b = " << b << "\n";
   std::cout<<"c = " << c << "\n";
   std::cout<<"|c| = " << c.mag() << "\n";
   std::cout<<"angle c = " << c.ang() << "rad or " << c.ang()*180/pi << "deg" << "\n";
 
+  std::cout<<"a==b : " << (a==b) << "\n";
+  std::cout<<"a!=b : " << (a!=b) << "\n";
+
+  ++c;
+  std::cout<<"++c = " << c << "\n";
+  c++;
+  std::cout<<"c++ = " << c << "\n";
+  
 
   return 0;
 }
